bc51dcf150b12723ca1977ef9e1e4f73a157039d
Yu Yu <qianchenzhumeng@live.cn>
2020-07-19 08:47:16 +0800

    收到 sn 的消息后新建 mqtt 连接转发给服务器

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
 extern crate data_management;
-use data_management::data_management::{data_base, device};
+extern crate rusqlite;
+extern crate mqtt;
+#[macro_use]
+extern crate log;
+extern crate clap;
+extern crate env_logger;
+extern crate time;
+extern crate uuid;
 
-fn main() {
-    let db = data_base::init_data_base("./", "a.db");
+use data_management::data_management::{data_base, device};
+use std::env;
+use std::io::Write;
+use std::net::TcpStream;
+use std::str;
+
+use clap::{App, Arg};
+
+use uuid::Uuid;
+
+use mqtt::control::variable_header::ConnectReturnCode;
+use mqtt::packet::*;
+use mqtt::{TopicFilter, TopicName};
+use mqtt::{Decodable, Encodable, QualityOfService};
+
+use std::thread;
+use std::time::Duration;
+
+fn generate_client_id() -> String {
+    format!("/MQTT/rust/{}", Uuid::new_v4())
+}
+
+fn init_data_base(path: &str, name: &str) -> Result<rusqlite::Connection, ()> {
+    let db = data_base::init_data_base(path, name);
 
     let db = match db {
         Ok(database) => database,
@@ -15,7 +44,225 @@ fn main() {
         Ok(changed) => println!("{} rows were changed", changed),
         Err(err) => println!("create DEVICE table failed: {:?}", err),
     }
+    Ok(db)
+}
 
+fn sn_msg_handle(sn: &str, topic: &str, msg: &str, server: &str) -> Result<(), ()>
+{
+    // 连接服务器
+    info!("Connecting to {:?} ... ", server);
+    let mut stream = TcpStream::connect(server).unwrap();
+    info!("Connected!");
+
+    info!("Client identifier {:?}", sn);
+    let mut conn = ConnectPacket::new("MQTT", sn);
+    conn.set_clean_session(true);
+    let mut buf = Vec::new();
+    conn.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+
+    let connack = ConnackPacket::decode(&mut stream).unwrap();
+    trace!("CONNACK {:?}", connack);
+
+    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
+        panic!(
+            "Failed to connect to server, return code {:?}",
+            connack.connect_return_code()
+        );
+    }
+    // 发布消息
+    let topic_name = TopicName::new(topic).unwrap();
+    let publish_packet = PublishPacket::new(topic_name, QoSWithPacketIdentifier::Level0, msg.clone());
+    let mut buf = Vec::new();
+    publish_packet.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+    // 断开连接
+    let disconn_pakdet = DisconnectPacket::new();
+    let mut buf = Vec::new();
+    disconn_pakdet.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+    Ok(())
+}
+
+fn main() {
+    env::set_var("RUST_LOG", env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()));
+    env_logger::init();
+
+    let db = match init_data_base("./", "test.db") {
+        Ok(database) => database,
+        Err(err) => {
+            panic!("Problem opening the database: {:?}", err)
+        },
+    };
+
+    let matches = App::new("sub-client")
+        .author("Y. T. Chung <zonyitoo@gmail.com>")
+        .arg(
+            Arg::with_name("SERVER")
+                .short("S")
+                .long("server")
+                .takes_value(true)
+                .required(true)
+                .help("MQTT server address (host:port)"),
+        ).arg(
+            Arg::with_name("SUBSCRIBE")
+                .short("s")
+                .long("subscribe")
+                .takes_value(true)
+                .multiple(true)
+                .required(true)
+                .help("Channel filter to subscribe"),
+        ).arg(
+            Arg::with_name("USER_NAME")
+                .short("u")
+                .long("username")
+                .takes_value(true)
+                .help("Login user name"),
+        ).arg(
+            Arg::with_name("PASSWORD")
+                .short("p")
+                .long("password")
+                .takes_value(true)
+                .help("Password"),
+        ).arg(
+            Arg::with_name("CLIENT_ID")
+                .short("i")
+                .long("client-identifier")
+                .takes_value(true)
+                .help("Client identifier"),
+        ).get_matches();
+
+    let server_addr = matches.value_of("SERVER").unwrap();
+    let client_id = matches
+        .value_of("CLIENT_ID")
+        .map(|x| x.to_owned())
+        .unwrap_or_else(generate_client_id);
+    let channel_filters: Vec<(TopicFilter, QualityOfService)> = matches
+        .values_of("SUBSCRIBE")
+        .unwrap()
+        .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
+        .collect();
+
+    let keep_alive = 60;
+
+    info!("Connecting to {:?} ... ", server_addr);
+    let mut stream = TcpStream::connect(server_addr).unwrap();
+    info!("Connected!");
+
+    info!("Client identifier {:?}", client_id);
+    let mut conn = ConnectPacket::new("MQTT", client_id);
+    conn.set_clean_session(true);
+    conn.set_keep_alive(keep_alive);
+    let mut buf = Vec::new();
+    conn.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+
+    let connack = ConnackPacket::decode(&mut stream).unwrap();
+    trace!("CONNACK {:?}", connack);
+
+    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
+        panic!(
+            "Failed to connect to server, return code {:?}",
+            connack.connect_return_code()
+        );
+    }
+
+    // const CHANNEL_FILTER: &'static str = "typing-speed-test.aoeu.eu";
+    info!("Applying channel filters {:?} ...", channel_filters);
+    let sub = SubscribePacket::new(10, channel_filters);
+    let mut buf = Vec::new();
+    sub.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+
+    loop {
+        let packet = match VariablePacket::decode(&mut stream) {
+            Ok(pk) => pk,
+            Err(err) => {
+                error!("Error in receiving packet {:?}", err);
+                continue;
+            }
+        };
+        trace!("PACKET {:?}", packet);
+
+        if let VariablePacket::SubackPacket(ref ack) = packet {
+            if ack.packet_identifier() != 10 {
+                panic!("SUBACK packet identifier not match");
+            }
+
+            info!("Subscribed!");
+            break;
+        }
+    }
+
+    let mut stream_clone = stream.try_clone().unwrap();
+    thread::spawn(move || {
+        let mut last_ping_time = 0;
+        let mut next_ping_time = last_ping_time + (keep_alive as f32 * 0.9) as i64;
+        loop {
+            let current_timestamp = time::get_time().sec;
+            if keep_alive > 0 && current_timestamp >= next_ping_time {
+                info!("Sending PINGREQ to broker");
+
+                let pingreq_packet = PingreqPacket::new();
+
+                let mut buf = Vec::new();
+                pingreq_packet.encode(&mut buf).unwrap();
+                stream_clone.write_all(&buf[..]).unwrap();
+
+                last_ping_time = current_timestamp;
+                next_ping_time = last_ping_time + (keep_alive as f32 * 0.9) as i64;
+                thread::sleep(Duration::new((keep_alive / 2) as u64, 0));
+            }
+        }
+    });
+
+    loop {
+        let packet = match VariablePacket::decode(&mut stream) {
+            Ok(pk) => pk,
+            Err(err) => {
+                //error!("Error in receiving packet {}", err);
+                //continue;
+                panic!("Error in receiving packet {}", err);
+            }
+        };
+        trace!("PACKET {:?}", packet);
+
+        match packet {
+            VariablePacket::PingrespPacket(..) => {
+                info!("Receiving PINGRESP from broker ..");
+            }
+            VariablePacket::PublishPacket(ref publ) => {
+                let msg = match str::from_utf8(&publ.payload_ref()[..]) {
+                    Ok(msg) => {
+                        let mut sn_topic = Vec::with_capacity(4);
+                        let v: Vec<&str> = publ.topic_name().split('/').collect();
+                        // 不足 4 的时候应该会有问题
+                        for i in 0..4 {
+                            sn_topic.push(v[i]);
+                        }
+                        if sn_topic[0] == "client" {
+                            let client_id = sn_topic[1];
+                            let topic = sn_topic[3];
+                            let mut sn_stream = stream.try_clone().unwrap();
+                            let r = sn_msg_handle(client_id, &topic, &msg, "127.0.0.1:1884");
+                            match r {
+                                Ok(_ok) => info!("handle sn msg successfully"),
+                                Err(err) => info!("handle sn msg failed: {:#?}", err),
+                            };
+                        }
+                        msg
+                    },
+                    Err(err) => {
+                        error!("Failed to decode publish message {:?}", err);
+                        continue;
+                    }
+                };
+                info!("PUBLISH ({}): {}", publ.topic_name(), msg);
+            }
+            _ => {}
+        }
+    }
+    /*
     let dev = device::Device {
         device_serial_number: 0,
         device_type: "I".to_string(),
@@ -26,7 +273,7 @@ fn main() {
     match device::create_device(&db, &dev) {
         Ok(created) => println!("{} device were created", created),
         //Err(err) => println!("create device {:#?} failed: {:?}", dev, err),
-        Err(err) => {},
+        Err(_err) => {},
     };
     
     let _db = match db.close() {
@@ -35,4 +282,5 @@ fn main() {
             panic!("Problem closing the database: {:?}", err)
         },
     };
+    */
 }
