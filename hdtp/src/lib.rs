c4ce377839eaaf0c161084a55de6db109280029f
qianchenzhumeng <qianchenzhumeng@live.cn>
2021-08-28 23:15:44 +0800

    summary: Fixes #4
    1. 解决数据模板引擎接收到的数据不完整问题
    2. 解决网关多次向服务器上传同一数据问题

--- hdtp/src/lib.rs
+++ hdtp/src/lib.rs
@@ -1,9 +1,9 @@
 
 use std::fmt;
 use log::error;
+use std::cell::RefCell;
 
 const START_OF_FRAM: u8 = 0x7E;
-
 const MAX_LENGTH: usize = 255;
 
 #[derive(Debug)]
@@ -22,10 +22,11 @@ enum FcsState {
 
 #[derive(Debug)]
 enum HdtpStatus {
-    Ok,
-    RxNotDone,     // 接收尚未完成
-    FrameError,     // 帧格式错误
-    FcsError,
+    WaitingForNextFrame,    // 准备接收下一帧数据
+    RxNotDone,              // 接收尚未完成
+    RxDone,                 // 完成接收
+    FrameError,             // 帧格式错误
+    FcsError,               // 校验码错误
 }
 
 #[derive(Debug)]
@@ -37,7 +38,7 @@ pub struct Hdtp {
     rx_fcs_state: FcsState,
     rx_frame_fcs: u16,
     rx_expected_fcs: u16,
-    status: HdtpStatus,
+    status: RefCell<HdtpStatus>,
 }
 
 impl fmt::Display for MechainState {
@@ -92,7 +93,7 @@ impl Hdtp {
             rx_fcs_state: FcsState::Start,
             rx_frame_fcs: 0,
             rx_expected_fcs: 0,
-            status: HdtpStatus::RxNotDone,
+            status: RefCell::new(HdtpStatus::WaitingForNextFrame),
         }
     }
 
@@ -102,6 +103,7 @@ impl Hdtp {
                 self.rx_frame_payload_bytes = 0;
                 self.rx_frame_length = 0;
                 self.rx_frame_fcs = 0;
+                *self.status.borrow_mut() = HdtpStatus::RxNotDone;
                 // 如果是开始标志，将状态迁移到“ReceivingLength”
                 if c == START_OF_FRAM {
                     self.rx_frame_state = MechainState::ReceivingLength;
@@ -111,11 +113,11 @@ impl Hdtp {
                 if c == 0 {
                     // 长度错误，将状态迁移到“SearchingForFlag”
                     self.rx_frame_state = MechainState::SearchingForFlag;
-                    self.status = HdtpStatus::FrameError;
+                    *self.status.borrow_mut() = HdtpStatus::FrameError;
                 } else {
                     self.rx_frame_length = c;
-                // 将状态迁移到“ReceivingPayload”
-                self.rx_frame_state = MechainState::ReceivingPayload;
+                    // 将状态迁移到“ReceivingPayload”
+                    self.rx_frame_state = MechainState::ReceivingPayload;
                 }
             },
             MechainState::ReceivingPayload => {
@@ -137,10 +139,10 @@ impl Hdtp {
                         self.rx_fcs_state = FcsState::Start;
                         match self.handle_msg() {
                             Ok(_) => {
-                                self.status = HdtpStatus::Ok;
+                                *self.status.borrow_mut() = HdtpStatus::RxDone;
                             },
                             Err(status) => {
-                                self.status = status;
+                                *self.status.borrow_mut() = status;
                             },
                         }
                         self.rx_frame_state = MechainState::SearchingForFlag;
@@ -197,16 +199,25 @@ impl Hdtp {
     }
 
     pub fn get_msg(&self) -> Result<String, ()> {
-        match self.status {
-            HdtpStatus::Ok => {
+        let mut status = self.status.borrow_mut();
+        match *status {
+            HdtpStatus::RxDone => {
                 let slice: &[u8; MAX_LENGTH] = &self.rx_frame_buffer;
                 let r = String::from_utf8(slice[0..self.rx_frame_payload_bytes as usize].to_vec());
                 match r {
-                    Ok(string) => return Ok(string),
-                    Err(_) => return Err(()),
+                    Ok(string) => {
+                        // 将状态置为等待接收下一帧数据
+                        *status = HdtpStatus::WaitingForNextFrame;
+                        Ok(string)
+                    },
+                    Err(_) => {
+                        Err(())
+                    },
                 }
             },
-            _ => return Err(()),
+            _ => {
+                Err(())
+            },
         }
     }
 }
@@ -325,11 +336,72 @@ mod tests {
             Ok(_) => {},
             //Err(_) => panic!("status: {:#?}, fcs: 0x{:X}(expected: 0x{:X}).", hdtp.status, hdtp.rx_frame_fcs, hdtp.rx_expected_fcs),
             Err(_) => {
-                match hdtp.status {
+                match *hdtp.status.borrow() {
                     crate::HdtpStatus::RxNotDone => {},
                     _ => panic!("{:#?}", hdtp),
                 }
             },
+        }
+    }
+
+    #[test]
+    /// 接收完一帧完整数据 "0123" 后，只能获取一次数据
+    fn test6() {
+        let s = "0123";
+        let mut hdtp = crate::Hdtp::new();
+        let v_data = [0x7E_u8, 4,
+            0x30, 0x31, 0x32, 0x33,
+            /* 2 字节 CRC */
+            0xBB, 0xBB
+        ];
+        for c in &v_data {
+            hdtp.input(*c);
+        }
+        // 接收到一帧完整的数据后，内部状态会变为 HdtpStatus::RxDone，此时可以拿到数据
+        match hdtp.get_msg() {
+            Ok(string) => {
+                assert_eq!(s, string);
+            },
+            Err(_) => panic!("status: {:#?}, fcs: 0x{:X}(expected: 0x{:X}).", *hdtp.status.borrow(), hdtp.rx_frame_fcs, hdtp.rx_expected_fcs),
+        }
+        // 获取一次数据后，内部状态需要变为 HdtpStatus::WaitingForNextFrame，再次获取，返回错误
+        match hdtp.get_msg() {
+            Ok(msg) => {
+                // 此处不应该拿到消息
+                panic!("status: {:#?}, msg: {}).", *hdtp.status.borrow(), msg);
+            },
+            Err(_) => {},
+        }
+    }
+
+    #[test]
+    /// 接收完一帧完整数据 "0123" 后，在下一帧数据已经开始接收但尚未接收完成前，尝试获取数据，返回错误
+    fn test7() {
+        let s = "0123";
+        let mut hdtp = crate::Hdtp::new();
+        let v_data = [0x7E_u8, 4,
+            0x30, 0x31, 0x32, 0x33,
+            /* 2 字节 CRC */
+            0xBB, 0xBB
+        ];
+        for c in &v_data {
+            hdtp.input(*c);
+        }
+        // 接收到一帧完整的数据后，内部状态会变为 HdtpStatus::Ok，此时可以拿到数据
+        match hdtp.get_msg() {
+            Ok(string) => {
+                assert_eq!(s, string);
+            },
+            Err(_) => panic!("status: {:#?}, fcs: 0x{:X}(expected: 0x{:X}).", *hdtp.status.borrow(), hdtp.rx_frame_fcs, hdtp.rx_expected_fcs),
+        }
+        // 重新递送一个字节，内部状态需要变为 HdtpStatus::RxNotDone，在此之后、帧接收未完成时，拿不到消息
+        hdtp.input(0);
+        match hdtp.get_msg() {
+            Ok(msg) => {
+                // 此处不应该拿到消息
+                panic!("status: {:#?}, msg: {}).", *hdtp.status.borrow(), msg);
+            },
+            Err(_) => {},
         }
     }
 }
